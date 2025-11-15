06e53422f01afd18f43ef19fde5cd3bfbd3966a2
Shiv Gandhi <sgandhi2169@gmail.com>
2025-11-14 21:46:08 -0500

    first commit

---
+++ Lab5/WorkingWithObjects.js
@@ -0,0 +1,58 @@
+const assignment = {
+    id: 1, title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-10-10", completed: false, score: 0,
+};
+const module = {
+    id: 1, name: "Intro to Node", 
+    description: "Learn node", course: "CS4550",
+};
+export default function WorkingWithObjects(app) {
+    const getAssignment = (req, res) => {
+        res.json(assignment); 
+    };
+    const getAssignmentTitle = (req, res) => {
+        res.json(assignment.title);
+    };
+    const setAssignmentTitle = (req, res) => {
+        const { newTitle } = req.params;
+        assignment.title = newTitle;
+        res.json(assignment);
+    };
+    const setAssignmentScore = (req, res) => {
+        const { score } = req.params;
+        assignment.score = parseInt(score);
+        res.json(assignment);
+    };
+    const setAssignmentCompleted = (req, res) => {
+        const { completed } = req.params;
+        assignment.completed === true;
+        res.json(assignment);
+    }
+    app.get("/lab5/assignment/score/:score", setAssignmentScore);
+    app.get("/lab5/assignment/completed/:completed", setAssignmentCompleted);
+    app.get("/lab5/assignment/title/:newTitle", setAssignmentTitle);
+    app.get("/lab5/assignment/title", getAssignmentTitle);
+    app.get("/lab5/assignment", getAssignment);
+
+    const getModule = (req, res) => {
+        res.json(module);
+    };
+    const getModuleName = (req, res) => {
+        res.json(module.name);
+    };
+    const setModuleName = (req, res) => {
+        const { newName } = req.params;
+        module.name = newName;
+        res.json(module);
+    };
+    const setModuleDescription = (req, res) => {
+        const { newDescription } = req.params;
+        module.description = newDescription;
+        res.json(module);
+    };
+    app.get("/lab5/module", getModule);
+    app.get("/lab5/module/name", getModuleName);
+    app.get("/lab5/module/name/:newName", setModuleName);
+    app.get("/lab5/module/description/:newDescription", setModuleDescription);
+};
