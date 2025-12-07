8c46bfa9000f461f6d68c3db7031f4497feff7cd
Shiv Gandhi <sgandhi2169@gmail.com>
2025-12-07 10:53:29 -0500

    fixing again

--- Kambaz/Database/users.js
+++ Kambaz/Database/users.js
@@ -1,4 +1,4 @@
-[
+export default[
     { "_id": "u1", "username": "alice", "password": "alice123", "firstName": "Alice", "lastName": "Wonderland", "role": "STUDENT" },
     { "_id": "u2", "username": "bob",   "password": "bob123",   "firstName": "Bob",   "lastName": "Builder",   "role": "FACULTY" }
   ]
